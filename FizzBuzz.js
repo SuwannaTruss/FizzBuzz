1f640942adecd0eb02d9f9a402c07461502cf583
Suwanna <suwanna.bgu@gmail.com>
2021-08-07 15:51:49 +0100

    completed strectch goal 1

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -15,8 +15,10 @@ for(let i=1; i<101; i++) {
     }
     console.log(result);
 } */
-// const result = [...Array(100)].map((_,i) => ((i+1)%3 === 0) ? "Fizz" : (i+1));
-// console.log(result);
+
+/* // Refactor
+const result = [...Array(100)].map((_,i) => ((i+1)%3 === 0) ? "Fizz" : (i+1));
+console.log(result); */
 
 
 /* //Part 4: add rule - print “Buzz” whenever the number is a multiple of 5
@@ -34,9 +36,31 @@ for(let i=1; i<101; i++) {
     console.log(result);
 } */
 
+/* // Refactor
 const result = [...Array(100)].map((_,i) => (((i+1)%3 === 0) && ((i+1)%5 === 0)) ? "FizzBuzz" 
     : ((i+1)%3 === 0) ? "Fizz" 
     : ((i+1)%5 === 0) ? "Buzz" 
     :(i+1));
 console.log(result);
+ */
 
+// Stretch Goal 1: If a number is a multiple of 7, print "Bang"
+//                 For numbers which are multiples of seven and three / five, 
+//                 append Bang to the previous result
+for(let i=1; i<101; i++) {
+    let result = "";
+    if ((i%3 === 0) && (i%5 === 0)) {
+        result = "FizzBuzz";
+    } else if (i%3 === 0) {
+        result = "Fizz";
+    } else if (i%5 === 0) {
+        result = "Buzz";
+    } 
+    if (i%7 === 0)  {
+        result += "Bang";
+    }
+    if (result === "") {
+        result = i;
+    }  
+    console.log(result);
+} 
