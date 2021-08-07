10dce48c5b635997421c9fb46f1cc4a494d6de94
Suwanna <suwanna.bgu@gmail.com>
2021-08-07 15:24:39 +0100

    refactor part 4

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -15,8 +15,8 @@ for(let i=1; i<101; i++) {
     }
     console.log(result);
 } */
-var result = [...Array(100)].map((_,i) => ((i+1)%3 === 0) ? "Fizz" : (i+1));
-console.log(result);
+// const result = [...Array(100)].map((_,i) => ((i+1)%3 === 0) ? "Fizz" : (i+1));
+// console.log(result);
 
 
 /* //Part 4: add rule - print “Buzz” whenever the number is a multiple of 5
@@ -33,3 +33,10 @@ for(let i=1; i<101; i++) {
     }
     console.log(result);
 } */
+
+const result = [...Array(100)].map((_,i) => (((i+1)%3 === 0) && ((i+1)%5 === 0)) ? "FizzBuzz" 
+    : ((i+1)%3 === 0) ? "Fizz" 
+    : ((i+1)%5 === 0) ? "Buzz" 
+    :(i+1));
+console.log(result);
+
