acaf51a75a113e39a9b4d0546a2bdff1126cb41d
Suwanna <suwanna.bgu@gmail.com>
2021-08-07 14:13:21 +0100

    initial

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,27 @@
-console.log("Hello everyone. Is it really a fine day today?");
+/ for loop to increment through 
+// ch eck if they are multiples of 3 and return fizz
+// check if they are multiples of 5 and return buzz
+// if they do both then return fizzbuzz
+// use console.log to print to the console.
+
+// console.log(test);
+// test = 2;
+// let test;
+// console.log(test);
+
+//var result = 1000;
+
+
+for (let i=1; i<101; i++) {
+    let result;
+    if ((i%3 === 0) && (i%5 === 0)) {
+        result = "FizzBuzz";
+    } else if (i%3 === 0) {
+        result = "Fizz";
+    } else if (i%5 === 0) {
+        result = "Buzz";
+    } else {
+        result = i;
+    }
+    console.log(result);
+}
