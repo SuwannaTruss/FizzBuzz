4d50c7ca4a7f882453b78b05064020a0ba2b7db2
Suwanna <suwanna.bgu@gmail.com>
2021-08-07 14:46:21 +0100

    completed part3

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,6 +1,17 @@
-// Part 1: 
-console.log("Hello World\n");
+/* // Part 1: 
+console.log("Hello World\n"); */
 
-//Part 2:
+/* //Part 2:
 [...Array(100)].forEach((_,i) =>console.log(i+1));
-console.log("\n");
+console.log("\n"); */
+
+// Part 3: print “Fizz“ whenever the number is a multiple of 3
+for(let i=1; i<101; i++) {
+    let result;
+    if (i%3 === 0) {
+        result = "Fizz";
+    } else {
+        result = i;
+    }
+    console.log(result);
+}
