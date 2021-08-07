fe155b8d65ef452b1e471e293ed0ca9b5135a6f6
Suwanna <suwanna.bgu@gmail.com>
2021-08-07 14:48:12 +0100

    completed part4

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -5,11 +5,26 @@ console.log("Hello World\n"); */
 [...Array(100)].forEach((_,i) =>console.log(i+1));
 console.log("\n"); */
 
-// Part 3: print “Fizz“ whenever the number is a multiple of 3
+/* // Part 3: print "Fizz" whenever the number is a multiple of 3
 for(let i=1; i<101; i++) {
     let result;
     if (i%3 === 0) {
         result = "Fizz";
+    } else {
+        result = i;
+    }
+    console.log(result);
+} */
+
+//Part 4: add rule - print “Buzz” whenever the number is a multiple of 5
+for(let i=1; i<101; i++) {
+    let result;
+    if ((i%3 === 0) && (i%5 === 0)) {
+        result = "FizzBuzz";
+    } else if (i%3 === 0) {
+        result = "Fizz";
+    } else if (i%5 === 0) {
+        result = "Buzz";
     } else {
         result = i;
     }
