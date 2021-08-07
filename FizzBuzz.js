9dc43b888f93e11de2e69129ec802b1610b62743
Suwanna <suwanna.bgu@gmail.com>
2021-08-07 15:56:44 +0100

    completed strectch goal 2

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -44,7 +44,7 @@ const result = [...Array(100)].map((_,i) => (((i+1)%3 === 0) && ((i+1)%5 === 0))
 console.log(result);
  */
 
-// Stretch Goal 1: If a number is a multiple of 7, print "Bang"
+/* // Stretch Goal 1: If a number is a multiple of 7, print "Bang"
 //                 For numbers which are multiples of seven and three / five, 
 //                 append Bang to the previous result
 for(let i=1; i<101; i++) {
@@ -58,6 +58,29 @@ for(let i=1; i<101; i++) {
     } 
     if (i%7 === 0)  {
         result += "Bang";
+    }
+    if (result === "") {
+        result = i;
+    }  
+    console.log(result);
+}  */
+
+// Stretch Goal 2: If a number is a multiple of 11, print "Bong". 
+//                 Only print "bong" evenif there are multiple factors for the number.
+for(let i=1; i<101; i++) {
+    let result = "";
+    if ((i%3 === 0) && (i%5 === 0)) {
+        result = "FizzBuzz";
+    } else if (i%3 === 0) {
+        result = "Fizz";
+    } else if (i%5 === 0) {
+        result = "Buzz";
+    } 
+    if (i%7 === 0)  {
+        result += "Bang";
+    }
+    if (i%11 === 0)  {
+        result = "Bong";
     }
     if (result === "") {
         result = i;
